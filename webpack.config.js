3c6e448bf10f147fd2f8f9f5745990b2f5f93d07
Alex Lockwood <alex.lockwood@gmail.com>
2016-05-13 12:02:46 -0700

    fix: get the basics working
    webpack.config.js: 
    just needed the 'loaders' to be nested inside  a 
    'module' object

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,11 @@ module.exports = {
   entry: [
     './src/index.js'
   ],
-  loaders: [
-    {test: /\.jsx?$/, include: `${__dirname}/src`, loader: 'babel-loader'}
-  ],
+  module: {
+    loaders: [
+      {test: /\.jsx?$/, include: `${__dirname}/src`, loader: 'babel-loader'}
+    ]
+  },
   output: {
     filename: 'bundle.js',
     path: `${__dirname}/dist`
